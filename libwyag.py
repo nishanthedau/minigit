e6f7659762b6a0dac09167ba3fe70ba60195d185
nishanthedau <fuz.nishant@gmail.com>
2026-03-19 11:55:20 +0530

    hash command

--- libwyag.py
+++ libwyag.py
@@ -237,3 +237,49 @@ def cat_file(repo, obj, fmt=None):
     
 def object_find(repo, name, fmt=None, follow=True):
     return name
+
+#hash command
+argsp=argsubparsers.add_parser(
+    "hash-object",
+    help="compute object ID and optionally creates a blob from file")
+
+argsp.add_argument(
+    "-t",
+    metavar="type",
+    dest="type",
+    choices=["blob","commit","tag","tree"],
+    default="blob",
+    help="Specify the type"
+)
+
+argsp.add_argument("-w",
+    dest="write",
+    action="store_true",
+    help="Actually write the object into the database"
+)
+
+argsp.add_argument("path",
+    help="Read object from <file>"
+)
+
+#implementation
+def cmd_hash_object(args):
+    if args.write:
+        repo=repo.find()
+    else:
+        repo = None
+        
+    with open(args.path, "rb") as fd:
+        sha = object(fd,args.type.encode(), repo)
+        print(sha)
+        
+def object_hash(fd, fmt, repo=None):
+    data = fd.read()
+    match fmt:
+        case b'commit' : obj=GitCommit(data)
+        case b'tree' : obj=GitTree(data)
+        case b'tag' : obj=GitTag(data)
+        case b'blob' : obj=GitBlob(data)
+        case _: raise Exception(f"unknown type {fmt}")
+        
+    return object_write(obj, repo)
